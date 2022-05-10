a4ce59afde8e383548301da34434995048e0ed37
dandanelek <danelek@inbox.lv>
2022-05-10 11:02:11 +0300

    Add project files.

---
+++ Projekts/Menu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekts
+{
+    public class Menu
+    { 
+        public string Name {get; set;}
+        public void Izveelne()
+        {
+            string caseSwitch = "${}";
+            switch (Name)
+            {
+                case string A:
+                    Console.WriteLine("Monday");
+                    break;
+                case string B:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case string C:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case string D:
+                    Console.WriteLine("Thursday");
+                    break;
+                case string E:
+                    Console.WriteLine("Friday");
+                    break;  
+               
+            }  
+           // Console.WriteLine($"Jūs esiet izvēlējies ārpus saraksta");
+        }
+}
+}
---
+++ Projekts/Program.cs
@@ -0,0 +1,39 @@
+﻿/*   Programma "C-Sharp Tutorial for beginners" tiek veidota, lai palīdzētu iesācējiem izprast/atkārtot  
+Microsoft radītas objektu orientētas programmēšanas valodas C-Sharp pamatus.
+
+  Šajā sadaļā tiek apskatīti(ar paskaidrojumiem):
+
+* Primitīvie datu tipi    (Data Types)
+* Aritmētiskie operatori  (Arithmetic Operators)
+* Relāciju operatori      (Relational Operators)
+* Loģiskie operatori      (Logical Operators)
+* Piešķiršanas operatori  (Unary Operators)
+*/
+/*
+Console.WriteLine("Kā tevi sauc ?");
+string userInput = Console.ReadLine();
+Console.WriteLine("Sveiks, " + userInput); */
+
+using Projekts;
+Menu menu = new Menu();
+Console.WriteLine("Izvēlies 1 2 3 4 5 ");
+string userInput = Console.ReadLine();
+menu.Name = userInput;
+menu.Izveelne();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
